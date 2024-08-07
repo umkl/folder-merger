73d25c13a3dde1e7f74f300d2861410dc10b0fc9
Michael <michael.ungar03@gmail.com>
2024-08-07 23:08:49 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const currentDirectory = __dirname;
+
+async function moveContents(srcDir, destDir) {
+	try {
+		const files = await fs.readdir(srcDir);
+
+		for (const file of files) {
+			const srcPath = path.join(srcDir, file);
+			const destPath = path.join(destDir, file);
+
+			const stat = await fs.stat(srcPath);
+
+			if (stat.isDirectory()) {
+				await moveContents(srcPath, destDir);
+			} else {
+				await fs.rename(srcPath, destPath);
+				console.log(`Moved ${srcPath} to ${destPath}`);
+			}
+		}
+	} catch (err) {
+		console.error(`Error processing directory ${srcDir}:`, err);
+	}
+}
+
+export async function moveAllSubdirectoryContentsToCurrentDirectory() {
+	try {
+		const files = await fs.readdir(currentDirectory);
+
+		for (const file of files) {
+			const srcPath = path.join(currentDirectory, file);
+
+			const stat = await fs.stat(srcPath);
+
+			if (stat.isDirectory()) {
+				await moveContents(srcPath, currentDirectory);
+			}
+		}
+	} catch (err) {
+		console.error(`Error reading current directory:`, err);
+	}
+}
